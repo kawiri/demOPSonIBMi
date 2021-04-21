36a09033b5d1f45d53d7ef9b6735d0df827d742d
Juan Gabriel Sanchez Posadas <kawiri@hotmail.com>
2021-04-21 09:53:23 -0600

    Se resolvio bug y se mejoro la presentacion.

--- users/admin.py
+++ users/admin.py
@@ -8,3 +8,36 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ('pk','user','address','phone_number','created')
     list_display_links = ('pk','user','created')
     list_editable = ('address','phone_number')
+
+    search_fields = (
+        'user__email',
+        'user__username',
+        'user__first_name',
+        'user__last_name',
+        'phone_number'
+    )
+    list_filter = (
+        'user__is_active',
+        'user__is_staff',
+        'created',
+        'modified',
+    )
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (
+                ('user','photo'),
+                ('sex')),
+        }),
+        ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography'),
+                ('address','city','country')
+            )
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),),
+        })
+    )
+    readonly_fields = ('created', 'modified',)
--- users/models.py
+++ users/models.py
@@ -13,15 +13,15 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.RESTRICT)
     website = models.URLField(max_length=200, blank=True)
     biography = models.TextField(blank=True)
-    phone_numer = models.CharField(max_length=10, blank=True)
+    phone_number = models.CharField(max_length=10, blank=True)
     address = models.CharField(blank=True, max_length=80)
     city = models.CharField(blank=True, max_length=80)
     country = models.CharField(blank=True, max_length=80)
     sex = models.CharField(blank=False, max_length=1, default='O', choices=sex_choice)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    borrar = models.CharField(max_length=30, blank=True)
-	#photo = models.ImageField(upload_to='users/picture', max_length=500, null=True)
+    #borrar = models.CharField(max_length=30, blank=True)
+    photo = models.ImageField(upload_to='users/picture', max_length=500, null=True)
 
 
     def __str__(self):
